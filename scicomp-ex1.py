a0c26b7db00624a2491c070c3d4f68f79be3afba
Ville Silvonen <silvoville@gmail.com>
2018-02-08 23:27:32 +0200

    Testataan, meneekö tämä githubiin

---
+++ scicomp-ex1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  8 19:45:06 2018
+
+@author: Ville
+"""
+
+"""
+0.  *Familiar with C++: basics, Python: intermediate but haven't used in a 
+    while, MATLAB: basic/intermediate, (LaTeX: intermediate)
+    *No background in software development
+    *No background in CS
+"""
+
+x=1 #
+print(id(x))
+x+=1
+print(id(x))
